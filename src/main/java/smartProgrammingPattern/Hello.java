fc94a9a6c59c41a9b9c7b8434e33e497c661c36d
pandudamera@gmail.com <pandudamera@gmail.com>
2020-06-17 15:43:33 +0530

    Second file

---
+++ src/main/java/smartProgrammingPattern/Hello.java
@@ -0,0 +1,11 @@
+package smartProgrammingPattern;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
